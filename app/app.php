2134076c012a6958d8caf52377d7dc4af8182670
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-10-02 14:01:28 -0700

    Add basic Silex functionality that lists stores and brands with the ability to add and delete stores.

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Collection.php";
-    require_once __DIR__."/../src/Item.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
 
     //Add symfony debug component and turn it on.
     use Symfony\Component\Debug\Debug;
@@ -21,5 +21,21 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
 
+    $app->post("/add_store", function() use ($app) {
+        $new_store = new Store($_POST['name']);
+        $new_store->save();
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    $app->post("/delete_store/{id}", function($id) use ($app) {
+        $store = Store::find($id);
+        $store->delete();
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    return $app;
 ?>
--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
 <?php
-    $web = require_once __DIR__."/../app/app.php"
-    $web->run();
+    $website = require_once __DIR__."/../app/app.php";
+    $website->run();
 ?>
